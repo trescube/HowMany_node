b35bd0195eaf16353530334ae288ab16f676c8b5
Stephen Hess <stephen.k.hess@gmail.com>
2017-01-27 16:27:05 -0500

    filled in README, changed src name

--- test/howmany.js
+++ test/howmany.js
@@ -1,7 +1,7 @@
 const test = require('tape');
 
-test('solve it', (t) => {
-  const solve = require('../src/howmany_filter_and_map');
+test('solve it with filter/reduce', (t) => {
+  const solve = require('../src/howmany_filter_and_reduce');
 
   t.deepEquals(solve(), [1, 7, 3, 2, 1, 1, 1, 2, 1, 1]);
   t.end();
